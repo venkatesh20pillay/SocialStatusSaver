6c2736c6a097552e2e554cab01f345a5ff0d67ad
unknown <venkatesh20pillay@gmail.com>
2023-04-13 00:59:17 +0530

    removed unsued code

--- app/src/main/java/com/statuses/statussavers/ImagesFragment.java
+++ app/src/main/java/com/statuses/statussavers/ImagesFragment.java
@@ -56,7 +56,6 @@ public class ImagesFragment extends Fragment {
     ActivityResultLauncher<Intent> someActivityResultLauncher;
     ArrayList<ModelClass> fileslist = new ArrayList<>();
     TextView placeholder;
-    BroadcastReceiver broadcastReceiver;
 
     @Nullable
     @org.jetbrains.annotations.Nullable
@@ -67,12 +66,9 @@ public class ImagesFragment extends Fragment {
         refreshLayout = (SwipeRefreshLayout) root.findViewById(R.id.swipe);
         refreshLayout2 = (SwipeRefreshLayout) root.findViewById(R.id.swipeRefreshLayout_emptyView);
         placeholder = (TextView) root.findViewById(R.id.empty_view);
-      //  setupOnActivityResult();
-        setupReciever();
         setupOnClickText();
-        setupLauncher();
-        checkPermission();
         setRefresh();
+        setuplayout();
         return root;
     }
 
@@ -80,21 +76,6 @@ public class ImagesFragment extends Fragment {
     public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
-    private void setupReciever() {
-
-         broadcastReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                // Get extra data included in the Intent
-                String message = intent.getStringExtra("message");
-                setuplayout();
-            }
-        };
-        LocalBroadcastManager.getInstance(getContext()).registerReceiver(broadcastReceiver,
-                new IntentFilter("custom-event-name"));
-    }
-
 
     private void setuplayout() {
         fileslist.clear();
@@ -115,9 +96,6 @@ public class ImagesFragment extends Fragment {
                String str = placeholder.getText().toString();
                if(str.equalsIgnoreCase(getString(R.string.nostatusimages))) {
                    openHowToUse();
-               }
-               else {
-                   checkPermission();
                }
             }
         });
@@ -143,12 +121,11 @@ public class ImagesFragment extends Fragment {
     private ArrayList<ModelClass> getData() {
 
         ModelClass f;
-        if(SDK_INT>=29) {
+        if(SDK_INT>29) {
             SharedPreferences sh = getActivity().getSharedPreferences("DATA_PATH", Context.MODE_PRIVATE);
             String uri = sh.getString("PATH", "");
-            //getContext().getContentResolver().takePersistableUriPermission(Uri.parse(uri), Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            getContext().getContentResolver().takePersistableUriPermission(Uri.parse(uri), Intent.FLAG_GRANT_READ_URI_PERMISSION);
             if (uri != null) {
-               // uri += "%2F.Statuses";
                 DocumentFile fileDoc = DocumentFile.fromTreeUri(getActivity().getApplicationContext(),Uri.parse(uri));
                 if (fileDoc.listFiles() != null){
                     DocumentFile[] files = fileDoc.listFiles();
@@ -167,8 +144,7 @@ public class ImagesFragment extends Fragment {
         File targetdir = new File(targetpath);
         files = targetdir.listFiles();
         if(files == null) {
-         //   String targetpath1 = "/storage/emulated/0"
-              String targetpath1 = Environment.getExternalStorageDirectory().getAbsolutePath()+"/Android/media/com.whatsapp/WhatsApp/Media/.Statuses";
+            String targetpath1 = Environment.getExternalStorageDirectory().getAbsolutePath()+"/Android/media/com.whatsapp/WhatsApp/Media/.Statuses";
             File targetdir1 = new File(targetpath1);
             files = targetdir1.listFiles();
         }
@@ -197,7 +173,6 @@ public class ImagesFragment extends Fragment {
             @Override
             public void onRefresh() {
                 refreshLayout.setRefreshing(true);
-                checkPermission();
                 {
                     new Handler().postDelayed(new Runnable() {
                         @Override
@@ -212,7 +187,6 @@ public class ImagesFragment extends Fragment {
             @Override
             public void onRefresh() {
                 refreshLayout2.setRefreshing(true);
-                checkPermission();
                 {
                     new Handler().postDelayed(new Runnable() {
                         @Override
@@ -224,106 +198,4 @@ public class ImagesFragment extends Fragment {
             }
         });
     }
-
-    protected void setupLauncher() {
-
-       someActivityResultLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                       checkIfAllowed();
-                    }
-                });
-    }
-
-    private void checkIfAllowed() {
-        if(SDK_INT>=29) {
-            boolean allowed = readDataFromPrefs();
-            if(allowed) {
-                setuplayout();
-            }
-        }
-    }
-
-
-    private void openDialog() {
-        Dialog dialog = new Dialog(getContext());
-                dialog.setContentView(R.layout.custom_dialog);
-                dialog.show();
-                Button button = dialog.findViewById(R.id.okbutton);
-                button.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        dialog.dismiss();
-                        try {
-                            // ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.MANAGE_EXTERNAL_STORAGE},2);
-                           // openSomeActivityForResult();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-    }
-
-    private void checkPermission() {
-
-        if(SDK_INT >= 29) {
-            boolean allowed = readDataFromPrefs();
-            if(allowed) {
-                setuplayout();
-            }
-            else {
-               // ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
-                //openDialog();
-               // getPermission();
-            }
-        }
-        else {
-            if(ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED) {
-                setuplayout();
-            }
-            else {
-                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
-                //Intent intent= new Intent(MainActivity.class,WRITE_EXTERNAL_STORAGE)
-                //someActivityResultLauncher.launch();
-            }
-       }
-
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.Q)
-    private void getPermission() {
-        StorageManager storageManager = (StorageManager) getActivity().getApplication().getSystemService(Context.STORAGE_SERVICE);
-        Intent intent = storageManager.getPrimaryStorageVolume().createOpenDocumentTreeIntent();
-        String path = "Android%2Fmedia%2Fcom.whatsapp%2FWhatsApp%2FMedia";
-        Uri uri = intent.getParcelableExtra("android.provider.extra.INITIAL_URI");
-        String scheme = uri.toString();
-        scheme = scheme.replace("/root/", "/tree/");
-        scheme += "%3A" + path;
-        uri = Uri.parse(scheme);
-        intent.putExtra("android.provider.extra.INITIAL_URI", uri);
-        intent.putExtra("android.content.extra.SHOW_ADVANCED", true);
-        startActivityForResult(intent, 4321);
-    }
-
-    private void setupOnActivityResult() {
-        someActivityResultLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if (result.getResultCode() == Activity.RESULT_OK) {
-                            // There are no request codes
-                            Uri tree = result.getData().getData();
-                            SharedPreferences sh = getActivity().getSharedPreferences("DATA_PATH", Context.MODE_PRIVATE);
-                            SharedPreferences.Editor ed = sh.edit();
-                            ed.putString("PATH", tree.toString());
-                            ed.apply();
-                            setuplayout();
-                        }
-                    }
-                });
-    }
-
 }
--- app/src/main/java/com/statuses/statussavers/MainActivity.java
+++ app/src/main/java/com/statuses/statussavers/MainActivity.java
@@ -45,63 +45,13 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 
         setContentView(binding.getRoot());
-        //actionBar = getSupportActionBar();
-//        if(getSupportActionBar()!=null) {
-//            getSupportActionBar().hide();
-//        }
-
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = binding.viewPager;
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
     }
-
-    private void checkPermission() {
-
-        if(SDK_INT>=29) {
-          //  getPermission();
-        }
-
-        else if(SDK_INT>23) {
-            if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED) {
-
-            }
-            else {
-                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-            }
-        }
-
-    }
-
-
-    @RequiresApi(api = Build.VERSION_CODES.Q)
-    private void getPermission() {
-        StorageManager storageManager = (StorageManager) getApplication().getSystemService(Context.STORAGE_SERVICE);
-        Intent intent = storageManager.getPrimaryStorageVolume().createOpenDocumentTreeIntent();
-        String path = "Android%2Fmedia%2Fcom.whatsapp%2FWhatsApp%2FMedia";
-        Uri uri = intent.getParcelableExtra("android.provider.extra.INITIAL_URI");
-        String scheme = uri.toString();
-        scheme = scheme.replace("/root/", "/tree/");
-        scheme += "%3A" + path;
-        uri = Uri.parse(scheme);
-        intent.putExtra("android.provider.extra.INITIAL_URI", uri);
-        intent.putExtra("android.content.extra.SHOW_ADVANCED", true);
-        startActivityForResult(intent, 4321);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode==RESULT_OK) {
-            Uri tree = data.getData();
-            SharedPreferences sh = getSharedPreferences("DATA_PATH", Context.MODE_PRIVATE);
-            SharedPreferences.Editor ed = sh.edit();
-            ed.putString("PATH", tree.toString());
-            ed.apply();
-        }
-    }
-
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -124,31 +74,5 @@ public class MainActivity extends AppCompatActivity {
     private void openHowToUse() {
         Intent intent = new Intent(this, HowToUse.class);
         startActivity(intent);
-    }
-
-    private void shareApp() {
-        try {
-            Intent shareIntent = new Intent(Intent.ACTION_SEND);
-            shareIntent.setType("text/plain");
-            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "My application name");
-            String shareMessage= "Let me recommend you this application\n\n";
-            shareMessage = shareMessage + "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID +"\n\n";
-            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
-            startActivity(Intent.createChooser(shareIntent, "choose one"));
-        } catch(Exception e) {
-            //e.toString();
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if(grantResults.length > 1 &&grantResults[0]==0 && grantResults[1]==0) {
-            Intent intent = new Intent("custom-event-name");
-            // You can also include some extra data.
-            intent.putExtra("message", "This is my message!");
-            LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
-        }
     }
 }
