31bbb4d6f1634a1e56e5d08cd1af0cb7db29d788
unknown <venkatesh20pillay@gmail.com>
2023-05-06 14:51:12 +0530

    added-reward-ad

--- app/src/main/java/com/statuses/statussavers/RewardActivity.java
+++ app/src/main/java/com/statuses/statussavers/RewardActivity.java
@@ -1,14 +1,136 @@
 package com.statuses.statussavers;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.OnUserEarnedRewardListener;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
 
 public class RewardActivity extends AppCompatActivity {
+
+    TextView descriptionView;
+    Button watchad;
+    RewardedAd rewardedAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reward);
+        if(getSupportActionBar()!=null) {
+            getSupportActionBar().setTitle("Remove Ad for free");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        descriptionView = (TextView) findViewById(R.id.description);
+        watchad = (Button) findViewById(R.id.watchad);
+        setLayout();
+        setOnClick();
+        initialiseAd();
+    }
+
+    private void setLayout() {
+        descriptionView.setText(R.string.rewardDescription);
+        watchad.setText("Watch Ad");
+    }
+
+    private void setOnClick() {
+        watchad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (rewardedAd != null) {
+                    loadAd();
+                }
+            }
+        });
+    }
+
+    private void initialiseAd() {
+        AdRequest adRequest = new AdRequest.Builder().build();
+        RewardedAd.load(this, "ca-app-pub-3940256099942544/5224354917",
+                adRequest, new RewardedAdLoadCallback() {
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error.
+                        rewardedAd = null;
+                    }
+
+                    @Override
+                    public void onAdLoaded(@NonNull RewardedAd ad) {
+                        rewardedAd = ad;
+                    }
+                });
+    }
+
+    private void loadAd() {
+        rewardedAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+            @Override
+            public void onAdClicked() {
+                // Called when a click is recorded for an ad.
+                int a = 10;
+            }
+
+            @Override
+            public void onAdDismissedFullScreenContent() {
+                // Called when ad is dismissed.
+                // Set the ad reference to null so you don't show the ad a second time.
+                rewardedAd = null;
+            }
+
+            @Override
+            public void onAdFailedToShowFullScreenContent(AdError adError) {
+                // Called when ad fails to show.
+                rewardedAd = null;
+            }
+
+            @Override
+            public void onAdImpression() {
+                // Called when an impression is recorded for an ad.
+                int a = 10;
+            }
+
+            @Override
+            public void onAdShowedFullScreenContent() {
+                // Called when ad is shown.
+                int a = 10;
+            }
+        });
+        if (rewardedAd != null) {
+            rewardedAd.show(this, new OnUserEarnedRewardListener() {
+                @Override
+                public void onUserEarnedReward(@NonNull RewardItem rewardItem) {
+                    // Handle the reward.
+                    int a = 10;
+                }
+            });
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(), "Error loading ad", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home)
+        {
+            finish();
+            return true;
+        }
+        else
+        {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
