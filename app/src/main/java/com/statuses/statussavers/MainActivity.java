3432e3ec03b10f896d9d0eedb8b32db0e2b4360b
Venkatesh Pillay <venkateshpillay@Venkateshs-Laptop.local>
2024-09-28 21:58:25 +0530

    code

--- app/src/main/java/com/statuses/statussavers/MainActivity.java
+++ app/src/main/java/com/statuses/statussavers/MainActivity.java
@@ -162,7 +162,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onAdLoadFailed(String s, MaxError maxError) {
-
+                maxAdView.loadAd();
             }
 
             @Override
